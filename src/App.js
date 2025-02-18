c60af7ae1889b74b0c6945fd6f65125d3a730cc4
Racha011 <ratshalingwaadivhaho106@gmail.com>
2025-02-18 10:37:07 +0200

    updated the package

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import AppRouter from "./router/AppRouter";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div className="app">
+      <Analytics />
       <AppRouter />
     </div>
   );
